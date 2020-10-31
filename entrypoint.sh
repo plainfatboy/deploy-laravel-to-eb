a9607714e7f3d86b3b18ee3d7a06631ea06425fb
Wasuwat Petprakob <wasuwatp18@gmail.com>
2020-11-01 01:52:56 +0700

    Throw an error if the deployment is not completed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,11 @@ cd ./application
 
 deployment_output=$(eb deploy)
 
+if [[ ! deployment_output =~ "deployment completed successfully" ]]
+then
+    echo "Failed to deploy!!! Please check the detailed error in AWS console."
+    exit 1
+fi
+
 export AWS_ACCESS_KEY_ID="****"
 export AWS_SECRET_ACCESS_KEY="****"
