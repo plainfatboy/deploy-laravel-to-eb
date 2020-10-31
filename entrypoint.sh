3e7f6ad00e019951f5688e6323c547965ca4e9da
Wasuwat Petprakob <wasuwatp18@gmail.com>
2020-11-01 04:14:56 +0700

    Update entrypoint
    Signed-off-by: Wasuwat Petprakob <wasuwatp18@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,15 @@ export AWS_SECRET_ACCESS_KEY="$2"
 
 cd /github/workspace
 
-deployment_output=$(eb deploy)
+eb deploy
 
-if [[ ! deployment_output =~ "deployment completed successfully" ]] :
-then
-    echo "Failed to deploy!!! Please check the detailed error in AWS console...."
-    exit 1
-fi
+# deployment_output=$(eb deploy)
+
+# if [[ ! deployment_output =~ "deployment completed successfully" ]] :
+# then
+#     echo "Failed to deploy!!! Please check the detailed error in AWS console...."
+#     exit 1
+# fi
 
 export AWS_ACCESS_KEY_ID="****"
 export AWS_SECRET_ACCESS_KEY="****"
