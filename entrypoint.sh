edf76689add8def52d9528370eb277456e968b73
Wasuwat Petprakob <wasuwatp18@gmail.com>
2020-11-08 02:26:22 +0700

    ...
    Signed-off-by: Wasuwat Petprakob <wasuwatp18@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,11 @@ export AWS_SECRET_ACCESS_KEY="$2"
 
 cd /github/workspace
 
+echo "Test message"
+
 echo $(ls /github/workspace)
+
+deployment_output="testing output"
 
 # eb init --region "ap-southeast-1" -p "PHP 7.4 running on 64bit Amazon Linux 2" -k "deploy" CakeFulfillment
 # eb deploy
