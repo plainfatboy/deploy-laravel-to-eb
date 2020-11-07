e198e14501d78d04cf3c75bb12e4985d86003c2d
Wasuwat Petprakob <wasuwatp18@gmail.com>
2020-11-08 03:03:35 +0700

    ...
    Signed-off-by: Wasuwat Petprakob <wasuwatp18@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,17 @@
 export AWS_ACCESS_KEY_ID="$1"
 export AWS_SECRET_ACCESS_KEY="$2"
 
-echo $AWS_ACCESS_KEY_ID
-echo $(ls)
+# deploymentoutput="testing output"
 
-deploymentoutput="testing output"
-
-# eb init --region "ap-southeast-1" -p "PHP 7.4 running on 64bit Amazon Linux 2" -k "deploy" CakeFulfillment
-# eb deploy
+eb init --region "ap-southeast-1" -p "PHP 7.4 running on 64bit Amazon Linux 2" -k "deploy" CakeFulfillment
+deploymentoutput=$(eb deploy)
 
 # deployment_output=$(eb deploy)
 
-# if [[ ! deployment_output =~ "deployment completed successfully" ]] :
-# then
-#     echo "Failed to deploy!!! Please check the detailed error in AWS console...."
-#     exit 1
-# fi
+if [[ ! deploymentoutput =~ "deployment completed successfully" ]] :
+then
+    echo "Failed to deploy!!! Please check the detailed error in AWS console...."
+    exit 1
+fi
 
 echo "::set-output name=deploymentoutput::$deploymentoutput"
-
-export AWS_ACCESS_KEY_ID="****"
-export AWS_SECRET_ACCESS_KEY="****"
