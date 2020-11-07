944e1833b17bab1be6a68f13e03db28bdda63942
Wasuwat Petprakob <wasuwatp18@gmail.com>
2020-11-08 03:07:00 +0700

    ...
    Signed-off-by: Wasuwat Petprakob <wasuwatp18@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,6 @@ export AWS_SECRET_ACCESS_KEY="$2"
 eb init --region "ap-southeast-1" -p "PHP 7.4 running on 64bit Amazon Linux 2" -k "deploy" CakeFulfillment
 deploymentoutput=$(eb deploy)
 
-# deployment_output=$(eb deploy)
-
 if [[ ! deploymentoutput =~ "deployment completed successfully" ]] :
 then
     echo "Failed to deploy!!! Please check the detailed error in AWS console...."
